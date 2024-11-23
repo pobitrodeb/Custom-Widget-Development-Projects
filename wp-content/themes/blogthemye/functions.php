0e1edf50b9b44ae12e3c662c95f5932f96743d33
Pobitro Deb <pobitrodebnath85@gmail.com>
2024-11-24 03:49:05 +0600

    HTML Template Upload Added

---
+++ wp-content/themes/blogthemye/functions.php
@@ -0,0 +1,44 @@
+<?php
+
+function blogthemye_setup(){
+        // Make theme available for translation
+        load_theme_textdomain('blogthemye', get_template_directory() . '/languages');
+
+        // Add default posts and comments RSS feed links to head
+        add_theme_support('automatic-feed-links');
+    
+        // Let WordPress manage the document title
+        add_theme_support('title-tag');
+    
+        // Enable support for Post Thumbnails on posts and pages
+        add_theme_support('post-thumbnails');
+    
+        // Register a primary menu
+        register_nav_menus(array(
+            'primary' => __('Primary Menu', 'blogthemye'),
+        ));
+}
+
+add_action('after_theme_setup', 'blogthemye_setup');
+
+function blogthemye_enqueue_scripts() {
+    // Stylesheets
+    wp_enqueue_style('blogthemye-font-awesome', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '4.7.0');
+    wp_enqueue_style('blogthemye-bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '4.0.0');
+    wp_enqueue_style('blogthemye-owl-carousel', get_template_directory_uri() . '/css/owl.css', array(), '2.3.4');
+    wp_enqueue_style('blogthemye-style', get_template_directory_uri() . '/css/style.css', array(), '1.0.0');
+    wp_enqueue_style('blogthemye-responsive', get_template_directory_uri() . '/css/responsive.css', array(), '1.0.0');
+
+    // Favicon
+    wp_enqueue_style('blogthemye-favicon', get_template_directory_uri() . '/images/favicon.ico', array(), null);
+
+    // Scripts
+    wp_enqueue_script('blogthemye-jquery', get_template_directory_uri() . '/js/jquery-2.1.4.js', array(), '2.1.4', true);
+    wp_enqueue_script('blogthemye-bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('blogthemye-jquery'), '4.0.0', true);
+    wp_enqueue_script('blogthemye-owl-carousel', get_template_directory_uri() . '/js/owl.carousel.min.js', array('blogthemye-jquery'), '2.3.4', true);
+    wp_enqueue_script('blogthemye-wow', get_template_directory_uri() . '/js/wow.js', array('blogthemye-jquery'), '1.1.2', true);
+    wp_enqueue_script('blogthemye-script', get_template_directory_uri() . '/js/script.js', array('blogthemye-jquery'), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'blogthemye_enqueue_scripts');
+
+
---
+++ wp-content/themes/blogthemye/sidebar.php
@@ -0,0 +1,119 @@
+<div class="col-md-3 col-sm-12 col-xs-12 sidebar-side">
+                    <div class="sidebar-content">
+
+                    <?php 
+                        if(is_active_sidebar('main_sidebar')){
+                            echo dynamic_sidebar('main-sidebar');
+                        }
+                    ?>
+
+                        <div class="sidebar-about centred">
+                            <div class="sidebar-title">ABOUT ME</div>
+                            <figure class="img-box"><img src="<?php echo get_template_directory_uri();?>/images/home/1.png" alt=""></figure>
+                            <h5 class="name">Jassy Beula</h5>
+                            <div class="text"><p>These days are all share them with me oh baby just sit right back and you will hear a tale a tale of a fateful trip that started.</p></div>
+                            <ul class="social-link">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fa fa-vimeo"></i></a></li>
+                            </ul>
+                        </div>
+
+
+                        <div class="sidebar-post">
+                            <?php
+                                if(is_active_sidebar('footer-sidebar')){
+                                    echo dynamic_sidebar('footer-sidebar');
+                                }
+                            ?> 
+                            <div class="sidebar-title">RECENT POST</div>
+                                <div class="single-post">
+                                    <div class="img-box"><a href="post1.html"><figure><img src="<?php echo get_template_directory_uri();?>/images/home/p1.jpg" alt=""></figure></a></div>
+                                    <h6><a href="post1.html">Fleeing from the Cylon tyre</a></h6>
+                                    <div class="text">JULY 09, 2018</div>
+                                </div>
+                                <div class="single-post">
+                                    <div class="img-box"><a href="post1.html"><figure><img src="<?php echo get_template_directory_uri();?>/images/home/p2.jpg" alt=""></figure></a></div>
+                                    <h6><a href="post1.html">Life support systems return</a></h6>
+                                    <div class="text">MAY 19, 2018</div>
+                                </div>
+                                <div class="single-post">
+                                    <div class="img-box"><a href="post1.html"><figure><img src="<?php echo get_template_directory_uri();?>/images/home/p3.jpg" alt=""></figure></a></div>
+                                    <h6><a href="post1.html">Eoner on a crusade</a></h6>
+                                    <div class="text">AUGUST 09, 2018</div>
+                                </div>
+                                <div class="single-post">
+                                    <div class="img-box"><a href="post1.html"><figure><img src="<?php echo get_template_directory_uri();?>/images/home/p4.jpg" alt=""></figure></a></div>
+                                    <h6><a href="post1.html">Aboard were expecting you</a></h6>
+                                    <div class="text">SEPTEMBER 10, 2018</div>
+                                </div>
+                                <div class="single-post">
+                                    <div class="img-box"><a href="post1.html"><figure><img src="<?php echo get_template_directory_uri();?>/images/home/p5.jpg" alt=""></figure></a></div>
+                                    <h6><a href="post1.html">Our dreams come true</a></h6>
+                                    <div class="text">OCTOBER 09, 2018</div>
+                                </div>
+                        </div>
+
+
+                        <div class="sidebar-newsletter centred">
+                            <div class="title"><i class="fa fa-envelope-o"></i>&nbsp;&nbsp;NEWSLETTER</div>
+                            <div class="text">These days are all share them wit me</div>
+                            <form action="#" method="post">
+                                <div class="form-group">
+                                    <input type="email" name="email" placeholder="Your Email Address" required="">
+                                    <button type="submit" class="btn-one">SUBSCRIBE</button>
+                                </div>
+                            </form>
+                        </div>
+
+
+                        <div class="sidebar-instagram">
+                            <div class="sidebar-title">INSTAGRAM</div>
+                            <ul class="img-list clearfix"> 
+                                <li><figure class="img-box"><a href="#"><img src="<?php echo get_template_directory_uri();?>/images/home/i1.jpg" alt=""></a></figure></li>
+                                <li><figure class="img-box"><a href="#"><img src="<?php echo get_template_directory_uri();?>/images/home/i2.jpg" alt=""></a></figure></li>
+                                <li><figure class="img-box"><a href="#"><img src="<?php echo get_template_directory_uri();?>/images/home/i3.jpg" alt=""></a></figure></li>
+                                <li><figure class="img-box"><a href="#"><img src="<?php echo get_template_directory_uri();?>/images/home/i4.jpg" alt=""></a></figure></li>
+                                <li><figure class="img-box"><a href="#"><img src="<?php echo get_template_directory_uri();?>/images/home/i5.jpg" alt=""></a></figure></li>
+                                <li><figure class="img-box"><a href="#"><img src="<?php echo get_template_directory_uri();?>/images/home/i6.jpg" alt=""></a></figure></li>
+                            </ul>
+                        </div>
+
+
+                        <div class="sidebar-categories">
+                            <div class="sidebar-title">CATEGORIES</div>
+                            <ul class="categories-list"> 
+                                <li><a href="#">Beauty<span>(3)</span></a></li>
+                                <li><a href="#">Dressing<span>(8)</span></a></li>
+                                <li><a href="#">Fitness <span>(7)</span></a></li>
+                                <li><a href="#">Lifestyle<span>(6)</span></a></li>
+                                <li><a href="#">Travel<span>(9)</span></a></li>
+                            </ul>
+                        </div>
+
+
+                        <div class="sidebar-img-content">
+                            <div class="single-item">
+                                <figure class="img-box"><img src="<?php echo get_template_directory_uri();?>/images/home/12.jpg" alt=""></figure>
+                                 <div class="inner-box">
+                                    <div class="content">
+                                        <div class="meta-text"><a href="#">Travel</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="single-item">
+                                <figure class="img-box"><img src="<?php echo get_template_directory_uri();?>/images/home/13.jpg" alt=""></figure>
+                                 <div class="inner-box">
+                                    <div class="content">
+                                        <div class="meta-text"><a href="#">MUSIC</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+                
